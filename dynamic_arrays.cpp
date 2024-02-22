0fbe2a5ba0ef5cbae603ae1ea2f1d630dd312d84
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-22 22:01:58 +0500

    Update dynamic_arrays.cpp

--- dynamic_arrays.cpp
+++ dynamic_arrays.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+using namespace std;
+int * returnArray(int n)
+{
+    int * arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the entry " << i << ": ";
+        cin >> arr[i];
+    }
+    return arr;
+}
+int main()
+{
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+    int * arr = returnArray(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "The enteries are " << arr[i] << endl;
+    }
+    delete [] arr;
+}
